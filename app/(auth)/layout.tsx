7b49e45b890ac0249b840225e7aad28bdaaf29fc
Ajaymaurya1008 <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-10-07 22:08:04 +0530

    final

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+
+
+const Authlayout = ({
+    children
+}: {
+    children: React.ReactNode
+}) => {
+
+    return (
+        <div className="flex justify-center items-center h-full">
+            {children}
+        </div>
+    )
+}
+
+export default Authlayout
